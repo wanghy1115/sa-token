3c60c80c4c03ca2a2c1d046e8d1e5da130d08230
wanghy <wanghy@damddos.com>
2025-01-24 17:28:40 +0800

    登录demo

--- src/main/java/com/why/satoken/controller/UsersController.java
+++ src/main/java/com/why/satoken/controller/UsersController.java
@@ -3,6 +3,7 @@ package com.why.satoken.controller;
 import cn.dev33.satoken.stp.StpUtil;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,7 +26,7 @@ public class UsersController {
      * @return
      */
     @RequestMapping("/doLogin")
-    public String doLogin(String username, String password) {
+    public String doLogin(@RequestParam String username, @RequestParam String password) {
         // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
         if("zhang".equals(username) && "123456".equals(password)) {
             StpUtil.login(10001);
@@ -41,8 +42,37 @@ public class UsersController {
      */
     @RequestMapping("/isLogin")
     public String isLogin() {
+        //StpUtil.isLogin()返回的是true和false，StpUtil.checkLogin()如果没有登录会抛出异常NotLoginException
         return "当前会话是否登录：" + StpUtil.isLogin();
     }
 
+    /**
+     * 登出
+     * @return
+     */
+    @RequestMapping("/doLogout")
+    public String doLogout() {
+        StpUtil.logout();
+        return "退出成功";
+    }
+
+    /**
+     * 获取当前会话的id
+     * @return
+     */
+    @RequestMapping("/getLoginId")
+    public String getLoginId() {
+        /**
+         * StpUtil.getTokenValue(): 56724a0b-6b5a-4b4a-ba56-8a5e28b80165
+         * StpUtil.getTokenName(): satoken
+         * StpUtil.getTokenInfo(): SaTokenInfo [tokenName=satoken, tokenValue=56724a0b-6b5a-4b4a-ba56-8a5e28b80165, isLogin=true, loginId=10001, loginType=login, tokenTimeout=2591994, sessionTimeout=2591994, tokenSessionTimeout=-2, tokenActiveTimeout=-1, loginDevice=default-device, tag=null]
+         * StpUtil.getLoginIdAsString(): 123456
+         */
+        System.out.println(String.format("StpUtil.getTokenValue(): %s" , StpUtil.getTokenValue()));
+        System.out.println(String.format("StpUtil.getTokenName(): %s" , StpUtil.getTokenName()));
+        System.out.println(String.format("StpUtil.getTokenInfo(): %s" , StpUtil.getTokenInfo()));
+        System.out.println(String.format("StpUtil.getLoginIdAsString(): %s" , StpUtil.getLoginIdAsString()));
+        return StpUtil.getLoginIdAsString();
+    }
 
 }
